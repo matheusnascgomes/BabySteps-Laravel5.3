9751abd20f500ad16daa7592b4e0c546162221ed
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-19 21:01:42 -0300

    Bug with sqlite migration

--- database/migrations/2018_02_19_235539_create_products_table.php
+++ database/migrations/2018_02_19_235539_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContactsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('tel');
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('value',5,2);
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('products');
     }
 }
