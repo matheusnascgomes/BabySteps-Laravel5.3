1976405bbeda4972fdc7597f55f232b71f081258
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-21 22:07:56 -0300

    structure table list courses finished

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Curso;
 
 class CourseController extends Controller
 {
     public function index()
     {
-      return view('admin.courses.index');
+      $registers = Curso::all();
+      return view('admin.courses.index',compact('registers'));
     }
 }
--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -6,16 +6,40 @@
 <div class="container center">
   <h3>Lista de Cursos</h3>
 
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-  </table>
+  <div class="row">
+    <table>
+      <thead>
+        <tr>
+          <th>Imagem</th>
+          <th>#id</th>
+          <th>Título</th>
+          <th>Descrição</th>
+          <th>Valor</th>
+          <th>Publicado</th>
+          <th>Ação</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($registers as $register)
+        <tr>
+          <td><img width="120" src="{{ assets($register->image) }}" alt="{{ $register->title }}"></td>
+          <td>{{ $register->id }}</td>
+          <td>{{ $register->title }}</td>
+          <td>{{ $register->description }}</td>
+          <td>{{ $register->value }}</td>
+          <td>{{ $register->published }}</td>
+          <td>
+            <a class="btn deep-orange" href="{{ route('admin.courses.update.show', $register->id) }}">Editar</a>
+            <a class="btn red" href="{{ route('admin.courses.delete',$register->id) }}"></a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  <div class="row">
+    <a class="btn blue" href="{{ route('admin.courses.add') }}" >Cadastrar</a>
+  </div>
 </div>
 
 @endsection
